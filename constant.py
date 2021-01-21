2619ecb203796a6bf011cf656200c9afc4c44cb5
pleen12 <pleen12@naver.com>
2021-01-21 17:19:45 +0900

    코드 정리

---
+++ constant.py
@@ -0,0 +1,13 @@
+import pygame as pg
+
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 800
+
+DEAD_LINE = 500
+
+BOX_WIDTH = 140
+BOX_HEIGHT = 32
+
+pg.init()
+TEXT_COLOR = pg.Color('lightskyblue3')
+TEXT_FONT = pg.font.Font(None, BOX_HEIGHT)
--- scene.py
+++ scene.py
@@ -1,18 +1,12 @@
-import datetime
-
 import pygame as pg
 import inputbox as ib
-import sys, time, random, sqlite3
-import word
+import sys, time, random, sqlite3, word, datetime
+from constant import *
 
 
 class SceneBase:
     def __init__(self):
         self.next = self
-        self.color = pg.Color('lightskyblue3')
-        self.font = pg.font.Font(None, 32)
-        self.cor_cnt = 0  # 정답 개수
-        self.life_time = 0
         # DB 생성
         self.conn = sqlite3.connect("./resource/records.db", isolation_level=None)
         self.cursor = self.conn.cursor()
@@ -20,7 +14,6 @@ class SceneBase:
         self.cursor.execute("CREATE TABLE IF NOT EXISTS records (" + \
                             "id INTEGER PRIMARY KEY AUTOINCREMENT," + \
                             "name TEXT , " + \
-                            "cor_cnt INTEGER, " + \
                             "life_time FLOAT, " + \
                             "score INTEGER, " + \
                             "regdate TEXT)")
@@ -41,15 +34,16 @@ class SceneBase:
 class LogoScene(SceneBase):
     def __init__(self):
         SceneBase.__init__(self)
-        self.name_box = ib.InputBox(330, 400, 140, 32)
+        self.box_color = pg.Color('lightskyblue3')
+        self.name_box = ib.InputBox((SCREEN_WIDTH - BOX_WIDTH) // 2, SCREEN_HEIGHT // 2, BOX_WIDTH, BOX_HEIGHT)
 
     def update(self):
         self.name_box.update()
 
     def render(self, screen):
-        screen.fill((255, 255, 255))
-        name_text = self.font.render('name', True, self.color)
-        screen.blit(name_text, (260, 405))
+        screen.fill('white')
+        name_text = TEXT_FONT.render('name', True, TEXT_COLOR)
+        screen.blit(name_text, (self.name_box.rect.x - 70, self.name_box.rect.y + 5))
         self.name_box.render(screen)
 
     def handle_event(self, event):
@@ -61,10 +55,11 @@ class LogoScene(SceneBase):
 class StageScene(SceneBase):
     def __init__(self):
         SceneBase.__init__(self)
-        self.input_box = ib.InputBox(400, 700, 140, 32)
-        self.cor_text = self.font.render('', True, self.color)
-        self.time_text = self.font.render('', True, self.color)
+        self.input_box = ib.InputBox((SCREEN_WIDTH - BOX_WIDTH) // 2, SCREEN_HEIGHT - 100, BOX_WIDTH, BOX_HEIGHT)
+        self.cor_text = TEXT_FONT.render('', True, TEXT_COLOR)
+        self.time_text = TEXT_FONT.render('', True, TEXT_COLOR)
         self.heart = 3
+        self.life_time = 0
         # 사운드 불러오기
         pg.mixer.init()
         self.correct_sound = pg.mixer.Sound("./sound/good.wav")
@@ -89,7 +84,6 @@ class StageScene(SceneBase):
         self.rain_words = []
         self.zen_time = time.time()
         self.score = 0
-        self.last_id = self.cursor.lastrowid
 
     def update(self):
         self.end = time.time()
@@ -98,24 +92,23 @@ class StageScene(SceneBase):
         self.input_box.update()
         for i in self.rain_words:
             i.update()
-            if i.y > 500:
+            if i.y > DEAD_LINE:
                 self.rain_words.remove(i)
                 pg.mixer.Sound.play(self.wrong_sound)  # 오답 사운드 재생
-                self.cor_text = self.font.render('Wrong!', True, self.color)
+                self.cor_text = TEXT_FONT.render('Wrong!', True, TEXT_COLOR)
                 self.heart -= 1
 
         if self.heart <= 0:
-            self.score = int(self.life_time) * self.cor_cnt
+            idx = self.cursor.execute('SELECT max(id) FROM records')
+            max_id = idx.fetchone()[0]
             self.cursor.execute(
                 "Update records "
-                "Set cor_cnt = ?, life_time = ?, score = ?, regdate = ? "
+                "Set life_time = ?, score = ?, regdate = ? "
                 "WHERE id = ?",
                 (
-                    self.cor_cnt, self.life_time, self.score,
-                    datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'), self.last_id
+                    self.life_time, self.score, datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'), max_id
                 )
             )
-
             # strftime('%Y-%m-%d %H:%M:%S') : 포맷 변환
 
             # 게임 실행해서 db기록되는지 확인
@@ -125,10 +118,10 @@ class StageScene(SceneBase):
             self.scene_change(GameOver())
 
     def render(self, screen):
-        screen.fill((0, 0, 0))
+        screen.fill('black')
         self.input_box.render(screen)
         self.ui_render(screen)
-        screen.blit(self.cor_text, (400, 600))
+        screen.blit(self.cor_text, (self.input_box.rect.x, self.input_box.rect.y - 50))
         for i in self.rain_words:
             i.render(screen)
 
@@ -138,21 +131,21 @@ class StageScene(SceneBase):
             for i in self.rain_words:
                 if str(self.input_box.text).strip() == str(i.text).strip():
                     pg.mixer.Sound.play(self.correct_sound)  # 정답 사운드 재생
-                    self.cor_text = self.font.render('Passed!', True, self.color)
-                    self.cor_cnt += 1  # 정답 개수 카운트
+                    self.cor_text = TEXT_FONT.render('Passed!', True, TEXT_COLOR)
+                    self.score += 20 * len(str(self.input_box.text).strip())
                     self.rain_words.remove(i)
                     break
                 if i == self.rain_words[-1]:
                     pg.mixer.Sound.play(self.wrong_sound)  # 오답 사운드 재생
-                    self.cor_text = self.font.render('Wrong!', True, self.color)
+                    self.cor_text = TEXT_FONT.render('Wrong!', True, TEXT_COLOR)
                     self.heart -= 1
 
             self.input_box.text = ""
 
     def ui_render(self, screen):
-        time_text = self.font.render('time: ' + format(self.life_time, '.3f'), True, self.color)
-        score_text = self.font.render('correct: ' + str(self.cor_cnt), True, self.color)
-        heart_text = self.font.render('heart: ' + str(self.heart), True, self.color)
+        time_text = TEXT_FONT.render('time: ' + format(self.life_time, '.3f'), True, TEXT_COLOR)
+        score_text = TEXT_FONT.render('score: ' + str(self.score), True, TEXT_COLOR)
+        heart_text = TEXT_FONT.render('heart: ' + str(self.heart), True, TEXT_COLOR)
         screen.blit(time_text, (260, 50))
         screen.blit(score_text, (260, 80))
         screen.blit(heart_text, (260, 110))
@@ -160,7 +153,7 @@ class StageScene(SceneBase):
     def zen_words(self):
         if self.zen_time + 1 < time.time():
             self.zen_time = time.time()
-            self.rain_words.append(word.Word(random.choice(self.words), 800, 800))
+            self.rain_words.append(word.Word(random.choice(self.words), SCREEN_WIDTH, SCREEN_HEIGHT))
 
 
 class GameOver(SceneBase):
@@ -174,7 +167,7 @@ class GameOver(SceneBase):
         pass
 
     def render(self, screen):
-        screen.fill((0, 0, 0))
+        screen.fill('black')
         screen.blit(self.end_text, (200, 100))
 
     def handle_event(self, event):
